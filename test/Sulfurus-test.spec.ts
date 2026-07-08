6f515b584f9c18ec9f23a00b30e53dba65c8c7e9
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-08 11:58:29 +0300

    Added test for Sulfurus

---
+++ test/Sulfurus-test.spec.ts
@@ -0,0 +1,16 @@
+﻿import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from "chai";
+
+describe('Gilded Rose', function () {
+
+    it('Check Backstage over', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 6, 7) ]);
+        var items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(7);
+        expect(items[0].sellIn).to.equal(6);
+    });
+
+});
