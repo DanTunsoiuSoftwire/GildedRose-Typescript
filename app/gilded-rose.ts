13de665ba1cba3c85b533b6efea609893812969e
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-08 11:24:24 +0300

    deleted unreachable code

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -50,29 +50,12 @@ export class GildedRose {
                     currentDecrease++;
                 }
                 currentItem.quality += currentDecrease;
+                currentItem.quality = Math.min(currentItem.quality, 50);
                 continue;
             }
 
-            if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    this.items[i].quality = this.items[i].quality - 1
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+            if (this.items[i].quality > 0) {
+                this.items[i].quality = this.items[i].quality - 1
             }
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
