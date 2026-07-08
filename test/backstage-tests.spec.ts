e64ad152eb40a0e6d33f7554cebb1f0b06ce1827
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-08 12:26:41 +0300

    Changed to have a single file for all scene tests

---
+++ test/backstage-tests.spec.ts
@@ -0,0 +1,36 @@
+﻿import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from "chai";
+
+describe('Gilded Rose', function () {
+
+    it('Check Backstage far', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 12, 5) ]);
+        var items = gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(11);
+        expect(items[0].sellIn).to.equal(8);
+    });
+
+    it('Check Backstage near', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 7, 10) ]);
+        var items = gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(20);
+        expect(items[0].sellIn).to.equal(3);
+    });
+
+    it('Check Backstage over', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 2, 13) ]);
+        var items = gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+        expect(items[0].sellIn).to.equal(-2);
+    });
+
+});
