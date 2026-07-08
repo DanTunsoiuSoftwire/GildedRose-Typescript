cb2cb8d602282fe8d910747df87b4e0a5326ecb6
DanTunsoiuSoftwire <Dan.Tunsoiu@softwire.com>
2026-07-08 11:44:49 +0300

    Added Unit Test for Aged-Brie

---
+++ test/aged-brie.spec.ts
@@ -0,0 +1,23 @@
+﻿import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from "chai";
+
+// Add a master test here
+describe('Gilded Rose', function () {
+
+    it('Golden Master Test', function() {
+        let exampleGildedRose = new GildedRose([new Item("Aged Brie", 5, 10)]);
+        let items = exampleGildedRose.updateQuality();
+        items = exampleGildedRose.updateQuality();
+        items = exampleGildedRose.updateQuality();
+        items = exampleGildedRose.updateQuality();
+        expect(items[0].quality).to.equal(14);
+        expect(items[0].sellIn).to.equal(1);
+        items = exampleGildedRose.updateQuality();
+        items = exampleGildedRose.updateQuality();
+        items = exampleGildedRose.updateQuality();
+        items = exampleGildedRose.updateQuality();
+        expect(items[0].quality).to.equal(21);
+        expect(items[0].sellIn).to.equal(-3);
+    });
+
+});
